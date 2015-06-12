c0f07c83a705f505d9a4bf8649430deac5f7baca
Dwayne Forde <dwayne.forde@gmail.com>
2015-06-11 22:47:20 -0400

    Initial Commit

---
+++ CF Commander/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CF Commander
+//
+//  Created by Dwayne Forde on 2015-06-11.
+//  Copyright (c) 2015 Dwayne Forde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
