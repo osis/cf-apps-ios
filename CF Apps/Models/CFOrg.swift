a61f11aa6877a8a84673fde86e9c833ba552d9a3
Dwayne Forde <dwayne.forde@gmail.com>
2015-12-15 08:10:30 -0500

    Implemented simple org filter

---
+++ CF Apps/Models/CFOrg.swift
@@ -0,0 +1,9 @@
+//
+//  CFOrg.swift
+//  CF Apps
+//
+//  Created by Dwayne Forde on 2015-12-10.
+//  Copyright © 2015 Dwayne Forde. All rights reserved.
+//
+
+import Foundation
---
+++ CF Apps/Models/CFSpace.swift
@@ -0,0 +1,9 @@
+//
+//  CFSpace.swift
+//  CF Apps
+//
+//  Created by Dwayne Forde on 2015-12-13.
+//  Copyright © 2015 Dwayne Forde. All rights reserved.
+//
+
+import Foundation
