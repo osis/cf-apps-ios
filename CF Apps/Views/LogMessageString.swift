8631676a6cc08d781e2798695f59e7aa998f04f9
Dwayne Forde <dwayne.forde@gmail.com>
2016-04-10 22:39:55 -0400

    Refactor log status message handling. Implemented reconnecting logic

---
+++ CF Apps/Views/LogMessageString.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UIKit
+
+class LogMessageString: NSMutableAttributedString {
+    static let font = UIFont(name: "Courier", size: 11.00)!
+    static let prefixColor = UIColor(red: 51/255, green: 140/255, blue: 231/255, alpha: 1.0)
+    static let outColor = UIColor.whiteColor()
+    static let errColor = UIColor.redColor()
+    
+    class func out(string: String) -> NSMutableAttributedString {
+        return message("", sourceID: "", message: string, type: LogMessage.MessageType.Out)
+    }
+    
+    class func err(string: String) -> NSMutableAttributedString {
+        return message("", sourceID: "", message: string, type: LogMessage.MessageType.Err)
+    }
+    
+    class func message(sourceName: String, sourceID: String, message: String, type: LogMessage.MessageType) -> NSMutableAttributedString {
+        let prefix = "\(sourceName)[\(sourceID)]:"
+        let text = NSMutableAttributedString(string: "\(prefix) \(message)\n\n", attributes: [NSFontAttributeName: font])
+        
+        let textString = NSString(string: text.string)
+        let prefixRange = textString.rangeOfString(prefix)
+        let messageRange = textString.rangeOfString(message)
+        let messageColor = (type == LogMessage.MessageType.Out) ? outColor : errColor
+        
+        text.addAttribute(NSForegroundColorAttributeName, value: prefixColor, range: prefixRange)
+        text.addAttribute(NSForegroundColorAttributeName, value: messageColor, range: messageRange)
+        
+        return text
+    }
+}
